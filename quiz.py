bcc51339fff9f8eb50e8ea6ec93d3806c9e88089
Diego Cortes <diegocortes4@gmail.com>
2025-02-04 14:10:18 -0500

    Initial commit

---
+++ quiz.py
@@ -0,0 +1,6 @@
+print('Welcome to the Python Basics Quiz!\n')
+print('You will answer 5 questions. Type A, B, C, or D to answer each question.')
+
+# Initialize score and question number
+score = 0
+question_number = 1
